55aadceba2ffe173a4e26926e1d4450fb35437a3
xx014939 <57679526+xx014939@users.noreply.github.com>
2022-03-16 21:53:30 +0000

    Homepage

--- js/view-nft.js
+++ js/view-nft.js
@@ -27,13 +27,13 @@ async function getAssets (walletID) {
     // Create AFRAME page 
     document.querySelector('.testing').innerHTML = 
     `
-    <a-scene>
+    <a-scene vr-mode-ui="enabled: true">
         
     <a-assets>
     <a-asset-item id="test" src="${image3D}"></a-asset-item>
     </a-assets>
     <a-entity position="-1 1 -3" gltf-model="#test"></a-entity>
-    
+
     <a-cylinder position="1 0.75 -3" radius="0.5" height="1.5" color="#FFC65D" src="${image2D}" id="cylinder"></a-cylinder>
     <a-plane position="0 0 -4" rotation="-90 0 0" width="4" height="4" color="#7BC8A4"></a-plane>
     <a-sky color="#ECECEC"></a-sky>
